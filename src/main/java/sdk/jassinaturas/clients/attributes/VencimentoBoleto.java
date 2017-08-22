60cbded611e3c44cdd78848bce98ab2d972595a1
Bruno Barros <barrosbruno@gmail.com>
2017-08-22 20:41:13 -0300

    String attributes,

--- src/main/java/sdk/jassinaturas/clients/attributes/VencimentoBoleto.java
+++ src/main/java/sdk/jassinaturas/clients/attributes/VencimentoBoleto.java
@@ -1,26 +1,26 @@
 package sdk.jassinaturas.clients.attributes;
 
 public class VencimentoBoleto {
-    private int day;
-    private int month;
-    private int year;
+    private String day;
+    private String month;
+    private String year;
 
-    public VencimentoBoleto(int day, int month, int year) {
+    public VencimentoBoleto(String day, String month, String year) {
 		super();
 		this.day = day;
 		this.month = month;
 		this.year = year;
 	}
 
-	public int getDay() {
+	public String getDay() {
         return day;
     }
 
-    public int getMonth() {
+    public String getMonth() {
         return month;
     }
 
-    public int getYear() {
+    public String getYear() {
         return year;
     }
 
